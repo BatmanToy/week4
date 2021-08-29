1d6466f986bb3ae95197836042874325971a8bb2
BatmanToy <89560708+BatmanToy@users.noreply.github.com>
2021-08-30 01:32:44 +1000

    Add files via upload

--- Assets/Scenes/LoadAssets.cs
+++ Assets/Scenes/LoadAssets.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LoadAssets : MonoBehaviour
 {
-    public GameObject redObj;
-    public GameObject blueObj;
+  
+    public GameObject redObj;   
+    [SerializeField] private GameObject blueObj;
    
     // Start is called before the first frame update
+    
+
     void Start()
     {
         //transform.position = new Vector3(2.0f, 0.0f, 0.0f);
